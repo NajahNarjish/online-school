9aef3778eb547de90edf2610bfd922dfcfbd765b
Syeda Najah Narjish <najah.masum@gmail.com>
2020-08-26 21:39:03 +0200

    file setup

---
+++ src/components/Course/Course.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Course = (props) => {
+    return (
+        <div>
+            <h1>Course Name</h1>
+            <p>Course duration:</p>
+            <h3>Price:</h3>
+        </div>
+    );
+};
+
+export default Course;
